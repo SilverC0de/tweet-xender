6009c03aceafd63f72bf736718ed5dbdfcf9d2e0
SilverC0de <silverg33k@gmail.com>
2020-10-10 12:10:26 +0100

    setting mvc up

---
+++ tweet.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+const api = express()
+
+
+require('./routes')(api)
+
+api.all('*', (request, response) => {
+  response.send('Tweet Xender running 😛😛')
+})
+
+
+api.listen(process.env.PORT, () => {
+  console.log(`Xend listening at http://localhost:${process.env.PORT}`)
+})
+
+module.exports = api
